921032b35450401a72c57deec322232f72149256
YassirJr <yassir.junior.03@gmail.com>
2025-01-10 21:42:16 +0100

    fix some css bugs in the UI for skills page

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -3,11 +3,11 @@ import {HoverEffect} from "@/components/ui/card-hover-effect";
 export default function Home() {
     return (
 
-        <div className="md:flex md:ps-44  justify-start flex-col items-start gap-4 px-1.5">
-            <h1 className="text-4xl mx-2 font-bold bg-clip-text text-transparent bg-gradient-to-b pt-20 from-neutral-300 to-neutral-500 bg-opacity-50">
+        <div className="md:flex md:ps-44 justify-start flex-col items-start gap-4 pb-12 px-1.5">
+            <h1 className="text-4xl mx-2 font-bold bg-clip-text text-transparent bg-gradient-to-b pt-10 md:pt-20 from-neutral-300 to-neutral-500 bg-opacity-50">
                 Skills
             </h1>
-                <HoverEffect />
+            <HoverEffect/>
         </div>
     );
 }
