aedd31e08ff692f6a5cee0059a7f68521cdfa08b
YassirJr <yassir.junior.03@gmail.com>
2025-01-12 12:33:39 +0100

    add paging bottom to the page

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -4,7 +4,7 @@ import {JSX} from "react";
 export default function SkillsPage() : JSX.Element {
     return (
 
-        <div className="md:flex md:ps-44 justify-start flex-col items-start gap-4 pb-12 px-1.5">
+        <div className="md:flex md:ps-44 justify-start flex-col items-start gap-4 pb-12 md:pb-32 px-1.5">
             <h1 className="text-4xl mx-2 font-bold bg-clip-text text-transparent bg-gradient-to-b pt-10 md:pt-20 from-neutral-300 to-neutral-500 bg-opacity-50">
                 Skills
             </h1>
