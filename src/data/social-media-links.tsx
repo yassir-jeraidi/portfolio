687ff8d724cac0c80fd6b992587b6088ff2eb825
YassirJr <yassir.junior.03@gmail.com>
2025-01-19 14:51:25 +0100

    create the social media links file

---
+++ src/data/social-media-links.tsx
@@ -0,0 +1,23 @@
+import { GithubIcon  , MailIcon , LinkedinIcon } from "lucide-react";
+import {Link} from "@/types";
+
+export const SOCIAL_MEDIA_LINKS : Link[] = [
+    {
+        id: 1,
+        title: "Github",
+        path: "https://github.com/YassirJr",
+        icon: <GithubIcon size={20}/>,
+    },
+    {
+        id: 2,
+        title: "LinkedIn",
+        path: "https://www.linkedin.com/in/yassir-jeraidi",
+        icon: <LinkedinIcon size={20}/>,
+    },
+    {
+        id: 3,
+        title: "Email",
+        path: "mailto:yassir.jeraidi@gmail.com",
+        icon: <MailIcon size={20}/>,
+    }
+]
