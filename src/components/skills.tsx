4f73735b97d70bde631e7dd3fff7603642d86136
YassirJr <yassir.junior.03@gmail.com>
2025-01-19 19:17:31 +0100

    fix the TracingBeam space in mobile device

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -5,7 +5,9 @@ export default function Skills(){
     return (
         <TracingBeam className="md:px-8 pb-12 md:pb-20">
             <div className="w-full min-h-screen md:py-20 flex flex-col flex-wrap justify-center items-center ">
-                <HoverEffect/>
+                <div className="ps-2 mx-auto relative z-10 w-full pt-20 md:pt-0">
+                    <HoverEffect/>
+                </div>
             </div>
         </TracingBeam>
     )
