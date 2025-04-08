8cbd769df6677b6d066ab6ad5a862a3543a86f7c
YassirJr <yassir.junior.03@gmail.com>
2025-04-08 23:39:07 +0100

    make the components support tailwind v4

--- src/components/ui/flaoting-dock.tsx
+++ src/components/ui/flaoting-dock.tsx
@@ -6,7 +6,7 @@ import {
     useMotionValue,
     useSpring,
     useTransform,
-} from "framer-motion";
+} from "motion/react";
 import Link from "next/link";
 import React, { useRef, useState } from "react";
 import type {Link as LinkType} from "@/types";
