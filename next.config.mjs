90f32683418a4d513bb7f8270383b63623a32e76
Yassir Jeraidi ! <98341652+yassir-jeraidi@users.noreply.github.com>
2025-04-13 17:48:54 +0100

    fix the django icon in skills

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.simpleicons.org',
-        port: '', // Leave empty for default HTTPS port
-        pathname: '/**', // Allow all paths under this domain
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
